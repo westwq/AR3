ee6b52f3fc6db9d4728d8fef5d5fb460b0f6b317
Wes mac <twq2@np.edu.sg>
2018-09-18 00:00:41 +0800

    Initial commit

---
+++ Assets/compass.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class compass : MonoBehaviour {
+    public float heading;
+    public Text txt;
+
+
+	// Use this for initialization
+	void Start () {
+        heading = 0;
+        txt.text = "starting";
+        StartCoroutine(InitializeLocation());
+
+    }
+    public IEnumerator InitializeLocation()
+    {
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("location disabled by user");
+            yield break;
+        }
+        // enable compass
+        Input.compass.enabled = true;
+        // start the location service
+        Debug.Log("start location service");
+        Input.location.Start(10, 0.01f);
+        // Wait until service initializes
+        int maxSecondsToWaitForLocation = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxSecondsToWaitForLocation > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxSecondsToWaitForLocation--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxSecondsToWaitForLocation < 1)
+        {
+            Debug.Log("location service timeout");
+            yield break;
+        }
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("unable to determine device location");
+            yield break;
+        }
+        Debug.Log("location service loaded");
+        yield break;
+    }
+    // Update is called once per frame
+    void Update () {
+        heading = Input.compass.trueHeading;
+
+        txt.text = heading.ToString();
+	}
+}
